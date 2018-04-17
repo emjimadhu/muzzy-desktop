41e2a51ce9d1fcbd4610d5ef99266c472e81da69
Batman <batman@localhost.localdomain>
2018-04-17 21:51:02 +0530

    Added Desktop NotificaTION

--- src/renderer/pages/tracks/scripts/vue/methods.js
+++ src/renderer/pages/tracks/scripts/vue/methods.js
@@ -1,4 +1,5 @@
 import NodeID3 from 'node-id3'
+// import notifier from 'node-notifier'
 
 import { readDir } from '@/helpers/fileActions'
 import Worker from '../myworker.worker.js'
@@ -118,6 +119,25 @@ export default {
         this.playingIndex = index
         this.songLoaded = false
         this.songs[index]['dataURI'] = dataURI
+        let myNotification = new Notification(info.title, {
+          body: `${info.artist} - ${info.album}`,
+          icon: info.cover,
+          image: info.cover
+        })
+        console.log(myNotification)
+        // notifier.notify(
+        //   {
+        //     title: info.title,
+        //     message: `${info.artist} - ${info.album}`,
+        //     icon: info.cover
+        //   },
+        //   function (err, response) {
+        //     console.log('response')
+        //     console.log(response)
+        //     console.log('error')
+        //     console.log(err)
+        //   }
+        // )
       }
     }
   },
